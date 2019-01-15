5e21409e57557c2778b74fb04e908d868748b5cf
Rafał Wesołowski <info@styleAx.de>
2019-01-15 15:57:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name shopware -d -p 80:80 -p 2222:22 -p 3306:3306 -p 9000:9000 wesolowski/shopware-php7
+docker run --name shopware -d -p 80:80 -p 2222:22 -p 3306:3306 wesolowski/shopware-php7
